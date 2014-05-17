fd7f8e70328753abcc0a6bbbbfb25d2c881a4ad9
wmtiger <bfstiger@gmail.com>
2014-05-17 23:13:21 +0800

    32323

---
+++ hdrdemo4/hdrdemo4/HDRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HDRAppDelegate.h
+//  hdrdemo4
+//
+//  Created by wei sm on 14-5-17.
+//  Copyright (c) 2014年 bf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hdrdemo4/hdrdemo4Tests/hdrdemo4Tests.m
@@ -0,0 +1,34 @@
+//
+//  hdrdemo4Tests.m
+//  hdrdemo4Tests
+//
+//  Created by wei sm on 14-5-17.
+//  Copyright (c) 2014年 bf. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hdrdemo4Tests : XCTestCase
+
+@end
+
+@implementation hdrdemo4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
